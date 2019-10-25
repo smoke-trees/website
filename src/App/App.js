51817754af7761170d25f51c699e18a97ea4726d
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-10-25 19:39:17 +0530

    Done project setup
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import BrowserRouter from "react-router-dom/modules/BrowserRouter";
-
 function App() {
     return (
-        <BrowserRouter>
             <div className="App">
                 <header className="App-header">
 
                 </header>
             </div>
-        </BrowserRouter>
     );
 }
 
