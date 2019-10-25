37c10e22590fc41d22ae22b75717d1b15e86c808
Anshuman Chhapolia <achhap.10.01@gmail.com>
2019-10-25 18:17:56 +0530

    Setup boilerplate-2
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import BrowserRouter from "react-router-dom/modules/BrowserRouter";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+
+                </header>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
