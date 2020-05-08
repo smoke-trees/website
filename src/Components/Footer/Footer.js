49f9502ff10713ab24b6cc401e16f4848a4ee967
Anshuman <achhap.10.01@gmail.com>
2020-05-08 23:09:53 +0530

    Fix nabar
    Signed-off-by: Anshuman <achhap.10.01@gmail.com>

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import './style.css'
+import ThemeContext from '../../theme'
 
 class Footer extends React.Component {
   render () {
     return (
-      <div className='footer'>
-        <div className='first-card-container'>
-          <div className='half-white' />
-          <div className='half-gray' />
-          <div className='first-card-wrapper'>
-            <div className='first-card'>
-              <h1>How can we help you?</h1>
+      <ThemeContext.Consumer>
+        {({ toggleTheme }) => (
+          <div className='footer'>
+            <div className='first-card-container'>
+              <div className='half-white' />
+              <div className='half-gray' />
+              <div className='first-card-wrapper'>
+                <div className='first-card'>
+                  <h1>Devs like dark theme.</h1>
+                  <div className='circle-button-wrapper'>
+                    <div onClick={toggleTheme} className='circle-button' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='second-card'>
+              <p>Lorem ipsumidolor sit amet consectetur adipisicing elit. Molestias, laborum est. Nesciunt eligendi dignissimos veritatis at asperiores magnam ipsa! Quos impedit consequatur temporibus recusandae consequuntur iure illo expedita architecto voluptatum.</p>
             </div>
           </div>
-        </div>
-        <div className='second-card'>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, laborum est. Nesciunt eligendi dignissimos veritatis at asperiores magnam ipsa! Quos impedit consequatur temporibus recusandae consequuntur iure illo expedita architecto voluptatum.</p>
-        </div>
-      </div>
+        )}
+      </ThemeContext.Consumer>
     )
   }
 }
