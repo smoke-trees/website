43962f11c41b640512bd9cbcbb96e02856dc9164
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-05-08 15:25:31 +0530

    made the navbar responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,45 +1,43 @@
-import React from 'react'
-import './style.css'
-import { Route, Link } from 'react-router-dom'
+import React from 'react';
+import './style.css';
+import { Route, Link } from 'react-router-dom';
 
 class Header extends React.Component {
-  render () {
-    return (
-      <div>
-        <div className='navbar-container'>
-          <div className='navbar'>
-            <div className='logo-container'>
-              <div className='logo'>
-                <Link to='/'>Smoketrees</Link>
-              </div>
-            </div>
-            <div style={{ flex: '1' }} />
-            <div className='navigation'>
-              <ul className='ul-list'>
-                <li>
-                  <Link to='/about'>About</Link>
-                </li>
-                <li>
-                  <Link to='/projects'>Projects</Link>
-                </li>
-                <li>
-                  <Link to='/team'>Team</Link>
-                </li>
-                <li>
-                  <Link to='/contact'>Contact</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <Route exact path='/' component={this.props.index} />
-        <Route path='/about' component={this.props.about} />
-        <Route path='/projects' component={this.props.projects} />
-        <Route path='/team' component={this.props.team} />
-        <Route path='/contact' component={this.props.contact} />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<header>
+						<a href='/' className='logo'>Smoketrees</a>
+					
+					<input type='checkbox' id='nav-toggle' className='nav-toggle' />
+					<nav>
+						<ul>
+							<li>
+								<a href='/about'>About</a>
+							</li>
+							<li>
+								<a href='/projects'>Projects</a>
+							</li>
+							<li>
+								<a href='/team'>Team</a>
+							</li>
+							<li>
+								<a href='/contact'>Contact</a>
+							</li>
+						</ul>
+					</nav>
+					<label for='nav-toggle' className='nav-toggle-label'>
+						<span></span>
+					</label>
+				</header>
+				<Route exact path='/' component={this.props.index} />
+				<Route path='/about' component={this.props.about} />
+				<Route path='/projects' component={this.props.projects} />
+				<Route path='/team' component={this.props.team} />
+				<Route path='/contact' component={this.props.contact} />
+			</div>
+		);
+	}
 }
 
-export default Header
+export default Header;
