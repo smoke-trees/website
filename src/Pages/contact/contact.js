032bc437012dcbc2fb0fe012105102e5da98a01f
Anshuman <achhap.10.01@gmail.com>
2020-05-08 18:16:19 +0530

    Add Dark theme toggle functionality
    Signed-off-by: Anshuman <achhap.10.01@gmail.com>

--- src/Pages/contact/contact.js
+++ src/Pages/contact/contact.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import './style.css'
 import contact from '../../vectors/undraw_terms_lso0.svg'
+import ThemeContext from '../../theme'
 
 class Contact extends React.Component {
   render () {
     return (
-      <div className='contact-page'>
-        <div className='container'>
-          <div className='content-page'>
-            <div className='image'>
-              <img src={contact} alt='' />
-            </div>
-            <div className='text-content'>
-              <div className='contact-content'>
-                <h3 className='office'>Office
-                  {/* <span className='branch' >Branch</span> */}
-                </h3>
-                <hr />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quasi.</p>
-                <hr />
-                <h3>+91XXXXXXXXXX</h3>
-                <h3>email@gmail.com</h3>
+      <ThemeContext.Consumer>
+        {({ theme }) => (
+          <div className={theme === 'dark' ? 'contact-page-dark' : 'contact-page'}>
+            <div className='container'>
+              <div className='content-page'>
+                <div className='image'>
+                  <img src={contact} alt='' />
+                </div>
+                <div className='text-content'>
+                  <div className='contact-content'>
+                    <h3 className='office'>Office
+                      {/* <span className='branch' >Branch</span> */}
+                    </h3>
+                    <hr />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quasi.</p>
+                    <hr />
+                    <h3>+91XXXXXXXXXX</h3>
+                    <h3>email@gmail.com</h3>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
+      </ThemeContext.Consumer>
     )
   }
 }
