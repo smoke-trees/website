1ed38272f6c2addebf8e47ead19b2456af3ede33
Anshuman <achhap.10.01@gmail.com>
2020-05-08 20:10:59 +0530

    Add arrows and make corousel responsive
    Signed-off-by: Anshuman <achhap.10.01@gmail.com>

---
+++ src/Pages/team/team-content.js
@@ -0,0 +1,155 @@
+export default [{
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}, {
+  name: 'Adarsh Singh',
+  devs: 'WEB',
+  description: `
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+         Optio quae aliquam et ullam repudiandae placeat rem laborum.
+         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
+         Tempore sit maiores obcaecati blanditiis.
+                    `,
+  github: 'https://github.com/adarshsingh87',
+  linkedin: 'https://linkedin.com/in/adarshsingh87'
+}]
