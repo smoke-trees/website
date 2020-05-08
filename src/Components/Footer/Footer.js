feb767ea5f7fbe611263f07cf3caa5a26de9025c
Anshuman <achhap.10.01@gmail.com>
2020-05-08 13:55:39 +0530

    Fix footer
    Signed-off-by: Anshuman <achhap.10.01@gmail.com>

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,14 @@ class Footer extends React.Component {
   render () {
     return (
       <div className='footer'>
-        <div className='first-card'>
-          <h1>How can we help you?</h1>
+        <div className='first-card-container'>
+          <div className='half-white' />
+          <div className='half-gray' />
+          <div className='first-card-wrapper'>
+            <div className='first-card'>
+              <h1>How can we help you?</h1>
+            </div>
+          </div>
         </div>
         <div className='second-card'>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, laborum est. Nesciunt eligendi dignissimos veritatis at asperiores magnam ipsa! Quos impedit consequatur temporibus recusandae consequuntur iure illo expedita architecto voluptatum.</p>
