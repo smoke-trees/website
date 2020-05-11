b08754de5b1bb8a0d5e37ec96b1283861bbee841
HelixW <shreyas.2000@hotmail.com>
2020-05-12 01:39:32 +0530

    Alignment changes and content addition

--- src/Pages/index/index.js
+++ src/Pages/index/index.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import './style.css'
-import logo from '../../vectors/logo.svg'
-import github from '../../vectors/github.svg'
-import versionControl from '../../vectors/version-control.svg'
-import designFeedback from '../../vectors/design_feedback.svg'
-import realTime from '../../vectors/real-time.svg'
-import ThemeContext from '../../theme'
-import 'aos/dist/aos.css'
+import React from 'react';
+import './style.css';
+import logo from '../../vectors/logo.svg';
+import github from '../../vectors/github.svg';
+import versionControl from '../../vectors/version-control.svg';
+import designFeedback from '../../vectors/design_feedback.svg';
+import realTime from '../../vectors/real-time.svg';
+import ThemeContext from '../../theme';
+import 'aos/dist/aos.css';
 
 class IndexPage extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     import('typed.js').then((TypedModule) => {
-      const Typed = TypedModule.default
+      const Typed = TypedModule.default;
       const typed = new Typed('#typed', {
         stringsElement: '#head-description',
-        typeSpeed: 30
-      })
-      return typed
-    })
+        typeSpeed: 30,
+      });
+      return typed;
+    });
     import('aos').then((AosModule) => {
-      AosModule.init()
-    })
+      AosModule.init();
+    });
   }
 
-  render () {
+  render() {
     return (
       <ThemeContext.Consumer>
         {({ theme, toggleTheme }) => (
@@ -31,27 +31,43 @@ class IndexPage extends React.Component {
             <div className='container'>
               <div className='landing'>
                 <div className='smoketrees'>
-                  <img alt='' src={logo} style={{ cursor: 'pointer' }} onClick={toggleTheme} />
+                  <img
+                    alt=''
+                    src={logo}
+                    style={{ cursor: 'pointer' }}
+                    onClick={toggleTheme}
+                  />
                 </div>
                 <div className='head ' id='head-description'>
-                  <p>We are a <span className='digital'>digital agency</span> <br />based on <span className='technology'>technology</span></p>
+                  <p>
+                    We are a <span className='digital'>digital agency</span>{' '}
+                    <br />
+                    based on <span className='technology'>technology</span>
+                  </p>
                   {/* <div className='btn'>
                                 Explore
                             </div> */}
                 </div>
                 <span id='typed' />
               </div>
-              <div className='tag' data-aos='fade-up'>We’re <span className='reinventing'>reinventing</span> offshore energy through sustainable, cutting-edge technology.</div>
+              <div className='tag' data-aos='fade-up'>
+                We <span className='reinventing'>build</span> what you{' '}
+                <span className='reinventing'>ideate</span>
+              </div>
               <div className='container-basic'>
                 <div className='description'>
                   <h1>GitHub</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque vitae malesuada bibendum quam aliquam. At dolor, sit eget commodo neque, accumsan nec volutpat. Sit posuere non mi duis id. </p>
+                  <p>
+                    Check out our project implementations here, we believe that
+                    our projects are robust and should be scrutinized by
+                    everyone so that they can see for themselves our raw work.
+                    PS: This website you're viewing? You can check out it's code
+                    on our github too!
+                  </p>
                   <div className='btn'>
                     <div className='elements'>
                       <img className='index-github-logo' src={github} alt='' />
-                      <div className='git'>
-                                        GitHub
-                      </div>
+                      <div className='git'>GitHub</div>
                     </div>
                   </div>
                 </div>
@@ -65,14 +81,18 @@ class IndexPage extends React.Component {
                 </div>
                 <div className='description alter'>
                   <div>
-                    <h1>Main Projects</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque vitae malesuada bibendum quam aliquam. At dolor, sit eget commodo neque, accumsan nec volutpat. Sit posuere non mi duis id. </p>
+                    <h1>Our Projects</h1>
+                    <p>
+                      CESS: Comprehensive Electoral Solutions Suite - an
+                      elections suite software which merges RFID and fingerprint
+                      recognition with extra layers of security which checks
+                      emotion when you vote while securing your transaction via
+                      a smart ethereum contract.
+                    </p>
                   </div>
                   <div className='btn'>
                     <div className='elements'>
-                      <div className='git'>
-                                        Know More
-                      </div>
+                      <div className='git'>Know More</div>
                     </div>
                   </div>
                 </div>
@@ -80,16 +100,22 @@ class IndexPage extends React.Component {
               <div className='container-basic'>
                 <div className='description'>
                   <h1>Clients</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque vitae malesuada bibendum quam aliquam. At dolor, sit eget commodo neque, accumsan nec volutpat. Sit posuere non mi duis id. </p>
+                  <p>
+                    KiVi Infotech is really pleased with the work delivered by
+                    SmokeTrees. Regular updates on GitHub for code development
+                    using Flutter platform made my work lot easier.
+                  </p>
                   <p
                     style={{
                       fontSize: '1.1em',
                       fontWeight: 'bold',
                       textAlign: 'right',
                       paddingRight: '20%',
-                      boxSizing: 'border-box'
+                      boxSizing: 'border-box',
+                      padding: 0,
                     }}
-                  >-Name
+                  >
+                    -Vishal Dhagat, KiVi Infotech
                   </p>
                 </div>
                 <div className='logo-svg'>
@@ -100,26 +126,13 @@ class IndexPage extends React.Component {
                 <div className='logo-svg'>
                   <img alt='' src={designFeedback} />
                 </div>
-                <div className='description alter'>
-                  <div>
-                    <h1>Main Projects</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque vitae malesuada bibendum quam aliquam. At dolor, sit eget commodo neque, accumsan nec volutpat. Sit posuere non mi duis id. </p>
-                  </div>
-                  <div className='btn '>
-                    <div className='elements'>
-                      <div className='git'>
-                                        Know More
-                      </div>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
         )}
       </ThemeContext.Consumer>
-    )
+    );
   }
 }
 
-export default IndexPage
+export default IndexPage;
