0d7a0e15ee3f8cb4ccf003590a4ce35b01026578
ATechnoHazard <amolele@gmail.com>
2020-05-12 22:30:54 +0530

    team-contend: Improve text semantics
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- src/Pages/team/team-content.js
+++ src/Pages/team/team-content.js
@@ -4,8 +4,8 @@ export default [{
   description: `
 Having worked with website design for the better part of 3 years,
  he has completed work on the frontends of various websites such as the Reboot hackathon,
-  SmokeTrees’ own website and the soon to be deployed SmokeTrees’ developed forum and blogposts. 
-  He has worked with react extensively and has operational experience.
+  SmokeTrees’ own website and the soon to be deployed Forum and Blog, developed by SmokeTrees.
+  He has worked with React extensively and has operational experience.
                     `,
   github: 'https://github.com/silverspear28',
   linkedin: 'https://www.linkedin.com/in/abhijay-kishore-a3b0b8152/'
@@ -15,7 +15,7 @@ Having worked with website design for the better part of 3 years,
   description: `
          Developer isn’t the word to fully capture what Amogh does, no matter the platform or stack, 
          he truly embodies the words “full-stack engineer”, and has worked on numerous projects
-          for VIT and Cybint. Member of DSC-VIT, he also has authored many illuminating blogposts on Medium..
+          for VIT and Cybint. Member of DSC-VIT, he also has authored many illuminating blogposts on Medium.
                     `,
   github: 'https://github.com/ATechnoHazard',
   linkedin: 'https://www.linkedin.com/in/amogh-lele-830131a4'
@@ -26,7 +26,7 @@ Having worked with website design for the better part of 3 years,
          The operational leader of this organization, 
          if you want anything fully integrated with many moving parts working together 
          seamlessly, you contact Anshuman. Truly a web-master, 
-         Anshuman has worked with many Series A startups as a consultant, helping them build their applications. .
+         Anshuman has worked with many Series A startups as a consultant, helping them build their applications.
                     `,
   github: 'https://github.com/achhapolia10',
   linkedin: 'https://linkedin.com/in/achhapolia10'
@@ -34,9 +34,9 @@ Having worked with website design for the better part of 3 years,
   name: 'Aryaman Sriram',
   devs: 'ML/AI',
   description: `
-         The AI visionary, always intent on pushing on research first,
+         The AI visionary, always intent on researching more,
           Aryaman has worked with Rover and Drone projects, truly pushing the boundaries of the field.
-          Being the Vice Captain of SEDS Projects VIT only adds to an illustrious knowledge base..
+          Being the Vice Captain of SEDS Projects VIT only adds to his illustrious knowledge base.
                     `,
   github: 'https://github.com/aryamansriram',
   linkedin: 'https://www.linkedin.com/in/aryaman-sriram-9a542b155/'
@@ -53,10 +53,10 @@ Having worked with website design for the better part of 3 years,
   name: 'Pranjal Srivastava',
   devs: 'APP DEV',
   description: `
- Being a Machine Learning Engineer and Flutter App Developer, 
+ A Machine Learning Engineer and Flutter App Developer, 
  Pranjal has worked as a part of many teams and startups to provide holistic solutions
- to many real world problems. His knowledge in hardware systems and software helps him connect various 
- domains of technology to innovate ideas.
+ to real world problems. His knowledge of both hardware and software helps him connect various 
+ domains of technology to solve challenging problems.
                     `,
   github: 'https://www.github.com/pranjalsrv',
   linkedin: 'https://www.linkedin.com/in/pranjalsrv'
@@ -67,8 +67,8 @@ Having worked with website design for the better part of 3 years,
   Sparsh lives and breathes Android development, 
   having Google Associate Certification for Android Development as 
   well as Nanodegree in Advanced Android. A 4.9-star freelancer on Fiverr,
-   he has a track record of creating Android Apps that are both well-received 
-   and commercially viable having published over 10 apps on Playstore with 10000+ downloads.
+  he has a track record of creating Android Apps that are both well-received 
+  and commercially viable having published over 10 apps on Playstore with 10000+ downloads.
                     `,
   github: 'https://github.com/reallyinvincible',
   linkedin: 'https://www.linkedin.com/in/sparshsri'
@@ -76,11 +76,11 @@ Having worked with website design for the better part of 3 years,
   name: 'Tanmay Thakur',
   devs: 'ML/AI',
   description: `
-         President of SmokeTrees,
-          Tanmay excels in the art of Deep Learning.
-           He has a great experience in developing and deploying models.
-            Having worked with companies like TCS, E&Y, YantraVision and 
-            Cybint he understands the needs and demands of the industry.
+        President of SmokeTrees,
+        Tanmay excels in the art of Deep Learning.
+        He has great experience in developing and deploying models.
+        Having worked with companies like TCS, E&Y, YantraVision and 
+        Cybint he understands the needs and demands of the industry.
                     `,
   github: 'https://github.com/lordtt13',
   linkedin: 'https://www.linkedin.com/in/tanmay-thakur-6bb5a9154'
@@ -88,11 +88,11 @@ Having worked with website design for the better part of 3 years,
   name: 'Ubaid Usmani',
   devs: 'ML/AI',
   description: `
-         A free thinker, Ubaid studies, teaches, researches,
-         posts about AI on a regular basis. You can find his courses
-          on Udemy and his teachings on various posts everywhere. 
-          He has also worked with TCS, LegalMind, Howitzer’s Technology.
-         He is a mentor for budding AI Engineers and leading the technical team at Developer Students Club VIT..
+        A free thinker, Ubaid studies, teaches, researches,
+        posts about AI on a regular basis. You can find his courses
+        on Udemy and his teachings on various posts everywhere. 
+        He has also worked with TCS, LegalMind, Howitzer’s Technology.
+        He is a mentor for budding AI Engineers and leading the technical team at Developer Students Club VIT..
                     `,
   github: 'https://github.com/Geek-ubaid/',
   linkedin: 'https://www.linkedin.com/in/ubaidusmani/'
@@ -100,10 +100,10 @@ Having worked with website design for the better part of 3 years,
   name: 'Unnikrishnan Mennon',
   devs: 'AI/ML',
   description: `
-            Unnikrishnan an all rounder having exprience with Reinforcement Learning, 
+            Unnikrishnan is an all rounder having exprience with Reinforcement Learning, 
             Game Dev and backend development. When he is not coding you can find him 
             answering questions on quora. He also likes to do some competitive coding
-            on free time.
+            in his free time.
                     `,
   github: 'https://github.com/7enTropy7/',
   linkedin: 'https://www.linkedin.com/in/unnikrishnan-menon-aa013415a/'
@@ -111,7 +111,7 @@ Having worked with website design for the better part of 3 years,
   name: 'Atri Chaturvedi',
   devs: 'WEB',
   description: `
-  Having started his journey from front-end development to gaining vast experience in backend development , 
+  Having started his journey from front-end development to gaining vast experience in backend development, 
   Atri has come a long way in his journey to become a full stack web developer.
   `,
   github: 'https://github.com/ATRI2107/',
@@ -119,25 +119,25 @@ Having worked with website design for the better part of 3 years,
 }, {
   name: 'Yash Kumar Verma',
   devs: 'WEB',
-  description: 'Yash is a GCI Finalist, open-source enthusiast, and a fluent backend developer, with expertise in Typescript, NodeJS, and PHP. He\'s passionate about cloud, automation, CI, and pipelines. MongoDB and MySQL are his choices for databases, and on odd days, he does ReactJS and cli scripting.',
+  description: 'Yash is a GCI Finalist, open-source enthusiast, and an experienced backend developer, with expertise in TypeScript, NodeJS, and PHP. He\'s passionate about cloud, automation, CI, and pipelines. MongoDB and MySQL are his choices for databases, and on odd days, he does ReactJS and CLI scripting.',
   github: 'https://github.com/yashkumarverma/',
   linkedin: 'https://www.linkedin.com/in/yash-kumar-verma/'
 }, {
   name: 'Pushkal Dwivedi',
   devs: 'ML/AI',
-  description: 'Pushkal is currently working in the field of Deep Learning. One of the major projects that he has been a part of , is analysis and classification of time series data collected from sensors, while working  as a research intern at TCS Innovation Labs. Apart from this, he also works on optimizing ML models and deploying them in web applications.',
+  description: 'Pushkal is currently working in the field of Deep Learning. One of the major projects that he has been a part of is analysis and classification of time series data collected from sensors, while working as a research intern at TCS Innovation Labs. Apart from this, he also works on optimizing ML models and deploying them in web applications.',
   github: 'https://github.com/CaptainRogers7',
   linkedin: 'https://www.linkedin.com/in/pushkal-dwivedi-cr7/'
 }, {
   name: 'Prakhar Mehta',
   devs: 'WEB',
-  description: 'A determined, commited, disciplined guy. Aiming to be a Full - stack web developer. Have been an astrophile since childhood. Passionate about sketching and space.',
+  description: 'A determined, commited, disciplined guy aiming to be a Full stack web developer. Has been an astrophile since childhood. Passionate about sketching and space.',
   github: 'http://github.com/prakharmehta',
   linkedin: 'http://linkedin.com/in/prakhar-mehta-983494180'
 }, {
   name: 'Ritik Gupta',
   devs: 'WEB',
-  description: 'Ritik Gupta is a passionate developer with decent experience in backend web development,he has worked with several cutting edge technologies in various projects and has also led the teams in various hackathons and has emerged victorious.',
+  description: 'Ritik Gupta is a passionate developer with experience in backend web development. He has worked with several cutting edge technologies in various projects and has also led the teams in various hackathons and has emerged victorious.',
   github: 'https://github.com/ritikgupt',
   linkedin: 'https://www.linkedin.com/in/ritik-gupta-494738178/'
 }, {
@@ -149,13 +149,13 @@ Having worked with website design for the better part of 3 years,
 }, {
   name: 'Rohan Arora',
   devs: 'DESIGN',
-  description: 'Rohan has excellent eye for design and having a technical base makes him a suitable for tech-based product designer. He is a valuable addition to SmokeTrees team. He is here to give your idea a beautiful image',
+  description: 'Rohan has an excellent eye for design and having a technical base makes him a suitable for tech-based product designer. He is a valuable addition to the SmokeTrees team. He is here to give your idea a beautiful image',
   linkedin: 'https://www.linkedin.com/in/rohanxdesign',
   drible: 'https://www.dribbble.com/rohanxdesign'
 }, {
   name: 'Hemanth Krishna',
   devs: 'APP DEV',
-  description: 'Hemanth is an experienced Android ROM and App Developer. Having developed many Android Apps on Java and Kotlin with latest features and clean codebase, his apps aim to solve day-to-day problems to make one’s life easier.',
+  description: 'Hemanth is an experienced Android ROM and App Developer. Having developed many Android Apps using Java and Kotlin with the latest features and a clean codebase, his apps aim to solve day-to-day problems to make one’s life easier.',
   linkedin: 'https://www.linkedin.com/in/darthbenro008/',
   github: ' https://github.com/DarthBenro008'
 }]
