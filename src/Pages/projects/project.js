bef7210ed9abff8f3cb17d374784e19ded32cfd6
HelixW <shreyas.2000@hotmail.com>
2020-05-12 14:56:50 +0530

    Content addition completed

--- src/Pages/projects/project.js
+++ src/Pages/projects/project.js
@@ -1,102 +1,79 @@
-import React from 'react'
-import './style.css'
-import Footer from '../../Components/Footer/Footer'
-import LandingBanner from '../../Components/LandingBanner/LandingBanner'
-import ColumnContent from '../../Components/ColumnContent/ColumnContent'
-import board from '../../vectors/undraw_scrum_board_cesn.svg'
-import ThemeContext from '../../theme'
+import React from 'react';
+import './style.css';
+import Footer from '../../Components/Footer/Footer';
+import LandingBanner from '../../Components/LandingBanner/LandingBanner';
+import ColumnContent from '../../Components/ColumnContent/ColumnContent';
+import board from '../../vectors/undraw_scrum_board_cesn.svg';
+import ThemeContext from '../../theme';
 
 class ProjectPage extends React.Component {
-  render () {
+  render() {
     return (
       <ThemeContext.Consumer>
         {({ theme }) => (
-          <div className={theme === 'dark' ? 'project-page dark' : 'project-page'}>
+          <div
+            className={theme === 'dark' ? 'project-page dark' : 'project-page'}
+          >
             <div className='container'>
               <LandingBanner image={board} heading='Projects'>
-                {' '}
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-      asperiores quo culpa velit ipsam excepturi dicta? Vel quibusdam
-      exercitationem, tenetur adipisci quae illum nobis tempora ipsam
-      maiores consectetur eligendi necessitatibus.{' '}
+                The Team behind SmokeTrees is focused on the motto of delivering
+                what you ideate. We have a team of skilled young minds who are
+                dedicated to serving and building the developers' community. Our
+                projects have been carried out with utmost sincerity and
+                diligence and will continue so. SmokeTrees is friendly,
+                flexible, versatile and packed with talent. If you have an idea
+                you want to bring to life, you have come to the right place. You
+                can find some of our projects mentioned below.
               </LandingBanner>
               <div className='project-content'>
-                <ColumnContent number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent heading='Atlas'>
+                  Total Surveillance for Infiltrators, a defense security
+                  solutions suite, sort documents on the fly for malcontent,
+                  configure drones for maximum area coverage, send
+                  communications via commo hubs, protected by swarm and
+                  blockchain. TLDR : Automated Solutions for Counter Insurgency.
                 </ColumnContent>
-                <ColumnContent className='column2' number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent className='column2' heading='Path Prediction'>
+                  A novel way to predict the optimal paths between two points
+                  and forsee future GPS data by studying old data using AI.
                 </ColumnContent>
-                <ColumnContent number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent heading='Uproar'>
+                  An application for accumulating all disaster updates in one
+                  forum, parsing them and manipulating the data so that only
+                  accurate information reaches the people. Features include SOS,
+                  mapping, video surveillance and many more.
                 </ColumnContent>
               </div>
               <div className='project-content'>
-                <ColumnContent className='column3' number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent heading='Spark'>
+                  A Smart Grid solution which compiles home networks and grids
+                  in an efficient manner, controlled by recurrent networks which
+                  predict distribution and consumption and also supported by an
+                  energy credit system. All running as microservices supporting
+                  each other.
                 </ColumnContent>
-                <ColumnContent number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent className='column3' heading='Voice Synthesis'>
+                  An implementation of Transfer Learning from Speaker
+                  Verification to Multispeaker Text-To-Speech Synthesis (SV2TTS)
+                  with a vocoder that works in real-time. SV2TTS is a
+                  three-stage deep learning framework that allows to create a
+                  numerical representation of a voice from a few seconds of
+                  audio, and to use it to condition text-to speech synthesis.
                 </ColumnContent>
-                <ColumnContent className='column3' number='01' heading='Lorem, Ipsum.'>
-                  {' '}
-       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-       minus, quasi omnis laborum dolore ab officia nihil! Maxime
-       debitis, architecto officiis dolores sapiente veniam possimus
-       repudiandae ex nisi earum cumque.Laborum repudiandae voluptatem,
-       labore nemo quis voluptate aliquid eveniet consequatur delectus
-       nostrum impedit similique magnam deserunt iste incidunt sint sed
-       non tempore. Dolor praesentium aliquid aut et provident? Pariatur,
-       alias.{' '}
+                <ColumnContent className='column3' heading='Gesture Drive'>
+                  An autonomous vehicle solution for people who are physically
+                  challenged, calibrates limb angle to drive steering wheel and
+                  operate pedals.
                 </ColumnContent>
               </div>
             </div>
+            <div className='spacer-sm'></div>
             <Footer />
           </div>
         )}
       </ThemeContext.Consumer>
-    )
+    );
   }
 }
 
-export default ProjectPage
+export default ProjectPage;
--- src/Pages/team/team-content.js
+++ src/Pages/team/team-content.js
@@ -1,10 +1,10 @@
 export default [
   {
-    name: 'Adarsh Singh',
+    name: 'Abhijay Kishore',
     devs: 'WEB',
     description: `Having worked with website design for the better part of 3 years, he has completed work on the frontends of various websites such as the Reboot hackathon, SmokeTrees’ own website and the soon to be deployed SmokeTrees’ developed forum and blogposts. He has worked with react extensively and has operational experience.`,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
+    github: 'https://github.com/silverspear28',
+    linkedin: 'https://www.linkedin.com/in/abhijay-kishore-a3b0b8152/',
   },
   {
     name: 'Amogh Lele',
@@ -26,135 +26,108 @@ export default [
     linkedin: 'https://linkedin.com/in/adarshsingh87',
   },
   {
-    name: 'Adarsh Singh',
+    name: 'Anshuman Chhapolia',
     devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
+    description: `The operational leader of this organization, if you want anything fully integrated with many moving parts working together seamlessly, you contact Anshuman. Truly a web-master, Anshuman has worked with many Series A startups as a consultant, helping them build their applications.`,
+    github: 'https://github.com/adarshsingh87https://github.com/achhapolia10',
+    linkedin: 'https://www.linkedin.com/in/achhapolia10',
+  },
+  {
+    name: 'Aryaman Sriram',
+    devs: 'ML/AI',
+    description: `The AI visionary, always intent on pushing on research first, Aryaman has worked with Rover and Drone projects, truly pushing the boundaries of the field. Being the Vice Captain of SEDS Projects VIT only adds to an illustrious knowledge base.`,
+    github: 'https://github.com/aryamansriram',
+    linkedin: 'https://github.com/aryamansriram',
+  },
+  {
+    name: 'Atharva Hudlikar',
+    devs: 'ML/AI',
+    description: `Having worked on Robotics, Computer Vision and Image Processing techniques, Atharva has developed many projects in the domain and has also worked with industries like Alpha ICT LLP and MellonAI.`,
+    github: 'https://www.github.com/Mastermind0100',
+    linkedin: 'https://www.linkedin.com/in/atharva-hudlikar/',
+  },
+  {
+    name: 'Pranjal Srivastava',
+    devs: 'APP DEV',
+    description: `Being a Machine Learning Engineer and Flutter App Developer, Pranjal has worked as a part of many teams and startups to provide holistic solutions to many real world problems. His knowledge in hardware systems and software helps him connect various domains of technology to innovate ideas.`,
+    github: 'https://www.github.com/pranjalsrv',
+    linkedin: 'https://www.linkedin.com/in/pranjalsrv',
+  },
+  {
+    name: 'Sparsh Srivastava',
+    devs: 'APP DEV',
+    description: `Sparsh lives and breathes Android development, having Google Associate Certification for Android Development as well as Nanodegree in Advanced Android. A 4.9-star freelancer on Fiverr, he has a track record of creating Android Apps that are both well-received and commercially viable having published over 10 apps on Playstore with 10000+ downloads.`,
+    github: 'https://github.com/reallyinvincible',
+    linkedin: 'https://www.linkedin.com/in/sparshsri',
+  },
+  {
+    name: 'Tanmay Thakur',
+    devs: 'ML/AI',
+    description: `President of SmokeTrees, Tanmay excels in the art of Deep Learning. He has a great experience in developing and deploying models. Having worked with companies like TCS, E&Y, YantraVision and Cybint he understands the needs and demands of the industry.`,
+    github: 'https://github.com/lordtt13',
+    linkedin: 'https://www.linkedin.com/in/tanmay-thakur-6bb5a9154',
+  },
+  {
+    name: 'Ubaid Usmani',
+    devs: 'ML/AI',
+    description: `A free thinker, Ubaid studies, teaches, researches, posts about AI on a regular basis. You can find his courses on Udemy and his teachings on various posts everywhere. He has also worked with TCS, LegalMind, Howitzer’s Technology. He is a mentor for budding AI Engineers and leading the technical team at Developer Students Club VIT.`,
+    github: 'https://github.com/Geek-ubaid/',
+    linkedin: 'https://www.linkedin.com/in/ubaidusmani/',
+  },
+  {
+    name: 'Unnikrishnan Mennon',
+    devs: 'AI/ML',
+    description: `Unnikrishnan an all rounder having exprience with Reinforcement Learning, Game Dev and backend development. When he is not coding you can find him answering questions on quora. He also likes to do some competitive coding on free time.`,
+    github: 'https://github.com/7enTropy7/',
+    linkedin: 'https://www.linkedin.com/in/unnikrishnan-menon-aa013415a/',
+  },
+  {
+    name: 'Atri Chaturvedi',
+    devs: 'WEB',
+    description: `Having started his journey from front-end development to gaining vast experience in backend development , Atri has come a long way in his journey to become a full stack web developer.`,
+    github: 'https://github.com/ATRI2107/',
+    linkedin: 'https://www.linkedin.com/in/atri-chaturvedi',
+  },
+  {
+    name: 'Yash Kumar Verma',
+    devs: 'WEB',
+    description: `Yash is a GCI Finalist, open-source enthusiast, and a fluent backend developer, with expertise in Typescript, NodeJS, and PHP. He's passionate about cloud, automation, CI, and pipelines. MongoDB and MySQL are his choices for databases, and on odd days, he does ReactJS and cli scripting.`,
+    github: 'https://github.com/yashkumarverma/',
+    linkedin: 'https://www.linkedin.com/in/yash-kumar-verma/',
+  },
+  {
+    name: 'Pushkal Dwivedi',
+    devs: 'ML/AI',
+    description: `Pushkal is currently working in the field of Deep Learning . Most of his works are based on Computer Vision. One of the major projects that he has been a part of , is analysis and classification of time series data collected from sensors, while working as a research intern at TCS Innovation Labs. Apart from this , he also works on optimizing ML models and deploying them in web applications.`,
+    github: 'https://github.com/CaptainRogers7',
+    linkedin: 'https://www.linkedin.com/in/pushkal-dwivedi-cr7/',
+  },
+  {
+    name: 'Prakhar Mehta',
+    devs: 'WEB',
+    description: `A determined, commited, disciplined guy. Aiming to be a Full - stack web developer. Have been an astrophile since childhood. Passionate about sketching and space.`,
+    github: 'http://github.com/prakharmehta',
+    linkedin: 'http://linkedin.com/in/prakhar-mehta-983494180',
+  },
+  {
+    name: 'Ritik Gupta',
+    devs: 'WEB',
+    description: `Ritik Gupta is a passionate developer with decent experience in backend web development,he has worked with several cutting edge technologies in various projects and has also led the teams in various hackathons and has emerged victorious.He is also a competitive coder and prefers practicing the same on platforms such as codeforces and codechef.He believes in the thought that limitation is only one's imagination and one can achieve anything with constant hardwork.`,
+    github: 'https://github.com/ritikgupt',
+    linkedin: 'https://www.linkedin.com/in/ritik-gupta-494738178/',
   },
   {
     name: 'Adarsh Singh',
     devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
+    description: `Adarsh is one of the most dedicated people we've worked with. He is really passionate about web-development. He is a wanderer and loves to explore and gain knowledge.`,
     github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
+    linkedin: 'https://www.linkedin.com/in/adarshsingh87/',
   },
   {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
-  },
-  {
-    name: 'Adarsh Singh',
-    devs: 'WEB',
-    description: `
-         Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         Optio quae aliquam et ullam repudiandae placeat rem laborum.
-         Excepturi cupiditate soluta ut? Quasi, rem. Porro, ratione.
-         Tempore sit maiores obcaecati blanditiis.
-                    `,
-    github: 'https://github.com/adarshsingh87',
-    linkedin: 'https://linkedin.com/in/adarshsingh87',
+    name: 'Hemanth Krishna',
+    devs: 'APP DEV',
+    description: `Hemanth is an experienced Android ROM and App Developer. Having developed many Android Apps on Java and Kotlin with latest features and clean codebase, his apps aim to solve day-to-day problems to make one’s life easier. He thrives to implement latest architecture and components and is focused on delivering quality work in stipulated time.`,
+    github: 'https://github.com/DarthBenro008',
+    linkedin: 'https://www.linkedin.com/in/darthbenro008/',
   },
 ];
